be8ce3ccbb265796e5200a8cc0f8e496a3a0887b
Julien Castelneau <julien.castelneau@ihu-liryc.fr>
2018-08-02 09:49:06 +0200

    medInriapublic : custom config

--- cmake/externals/projects_modules/medInria.cmake
+++ cmake/externals/projects_modules/medInria.cmake
@@ -61,8 +61,8 @@ EP_SetDirectories(${ep}
 ## Define repository where get the sources
 ## #############################################################################
 
-set(git_url ${GITHUB_PREFIX}Inria-Asclepios/medInria-public.git)
-set(git_tag master)
+set(git_url ${GITHUB_PREFIX}juliencastelneau/medInria-public.git)
+set(git_tag PaintBrushWithInterpolation)
 
 
 ## #############################################################################
